44d41590294ed8b73344862597d76269689250dd
Lucascg <lucascgiacomin16@gmail.com>
2025-02-26 12:01:27 -0300

    implementando header dinamico

--- Angular/Aumigos_Pet_Shop/src/app/app.component.ts
+++ Angular/Aumigos_Pet_Shop/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +10,30 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Aumigos';
-  sessionstorage: any=sessionStorage?.getItem('currentUser')
+  isLogin: boolean
+  private routerSub: Subscription;
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.routerSub = this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event: any) => {
+      this.isLogin = this.verifyIfLoginPage(event.url);
+    });
+  }
+  ngOnDestroy(): void {
+    if(this.routerSub){
+      this.routerSub.unsubscribe();
+    }
+  }
+
+  private verifyIfLoginPage(url: string){
+    if(url.includes('/login')){
+      return true;
+    }
+    return false;
+  }
 }
