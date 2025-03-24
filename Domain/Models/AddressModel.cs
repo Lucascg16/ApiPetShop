188fe6b7ed8f032e8479d80a426dab510393c840
Lucascg <lucascgiacomin16@gmail.com>
2025-03-24 14:28:01 -0300

    ajustando os tamanhos do address e alinhando banco de dados

--- Domain/Models/AddressModel.cs
+++ Domain/Models/AddressModel.cs
@@ -6,9 +6,14 @@ public class AddressModel
 {
     [Key]
     public int Id { get; set; }
+    [MaxLength(255)]
     public string? Street { get; set; }
+    [MaxLength(255)]
     public string? City { get; set; }
+    [MaxLength(255)]
     public string? State { get; set; }
+    [MaxLength(255)]
     public string? Neighborhood { get; set; }
+    [MaxLength(100)]
     public string? ZipCode { get; set; } 
 }
